b29dc3d19bc9d65967dfdeea4022f653d7eb15f9
Jinzulen <Jinzu@protonmail.ch>
2019-02-13 13:24:38 +0100

    Bulky code, useless code...

--- src/utilities.js
+++ src/utilities.js
@@ -7,45 +7,38 @@ const HTTPS = require("https");
 
 exports.callAPI = function (Path, Callback)
 {
-      try
-      {
-            HTTPS.get(Path, (Result) => {
-                  let Error, rawData = "";
-                  const Code = Result.statusCode, Type = Result.headers["content-type"];
+      HTTPS.get(Path, (Result) => {
+            let Error, rawData = "";
+            const Code = Result.statusCode, Type = Result.headers["content-type"];
 
-                  if (Code !== 200)
-                  {
-                        Error = `# [TenorJS] Could not send request @ ${Path} - Status Code: ${Code}`;
-                        Error.code = "ERR_REQ_SEND";
-                  } else if (Type.indexOf("application/json") === -1) {
-                        Error = `# [TenorJS] Content received isn't JSON. Type: ${Type}`;
-                        Error.code = "ERR_RES_NOT";
-                  }
+            if (Code !== 200)
+            {
+                  Error = `# [TenorJS] Could not send request @ ${Path} - Status Code: ${Code}`;
+                  Error.code = "ERR_REQ_SEND";
+            } else if (Type.indexOf("application/json") === -1) {
+                  Error = `# [TenorJS] Content received isn't JSON. Type: ${Type}`;
+                  Error.code = "ERR_RES_NOT";
+            }
 
-                  if (Error)
+            if (Error) { Result.resume(); Callback(Error); };
+
+            Result.setEncoding("utf8");
+            Result.on("data", function (Buffer) { rawData += Buffer; });
+
+            Result.on("end", () => {
+                  let Data = null, Error = null;
+
+                  try
                   {
-                        Result.resume();
-                        Callback(Error);
+                        Data = JSON.parse(JSON.stringify(rawData));
+                  } catch (unusedError) {
+                        Error = "# [TenorJS] Failed to parse retrieved JSON.";
+                        Error.code = "ERR_JSON_PARSE";
                   };
 
-                  Result.setEncoding("utf8");
-                  Result.on("data", function (Buffer) { rawData += Buffer; });
-
-                  Result.on("end", () => {
-                        let Data = null, Error = null;
-
-                        try
-                        {
-                              Data = JSON.parse(JSON.stringify(rawData));
-                        } catch (unusedError) {
-                              Error = "# [TenorJS] Failed to parse retrieved JSON.";
-                              Error.code = "ERR_JSON_PARSE";
-                        };
-
-                        Callback(Error, Data);
-                  });
+                  Callback(Error, Data);
             });
-      } catch (E) { throw E; };
+      });
 };
 
 exports.manageAPI = function (Endpoint, Callback, pResolve, pReject)
