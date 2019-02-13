8968692df21e2e09b8dcbeb76e630172234903d8
Jinzulen <Jinzu@protonmail.ch>
2019-02-13 13:19:03 +0100

    More redundant code removal.

--- src/search.js
+++ src/search.js
@@ -6,54 +6,42 @@
 
 module.exports = function (Credentials, Utilities)
 {
-      const Methods     = {},
-            Key         = Credentials.Key,
-            Locale      = Credentials.Locale,
-            Filter      = Credentials.Filter.toLowerCase(),
-            MediaFilter = Credentials.MediaFilter;
+      const Methods     = {}, Gateway = "https://api.tenor.com/v1",
+            Key         = Credentials.Key, Locale = Credentials.Locale,
+            Filter      = Credentials.Filter.toLowerCase(), MediaFilter = Credentials.MediaFilter;
 
       Methods.random = Methods.query = function (Term, Limit, Callback)
       {
             return new Promise((Resolve, Reject) => {
-                  const Endpoint = `https://api.tenor.com/v1/random?q=${Term}&key=${Key}&limit=${Limit}&contentfilter=${Filter}&locale=${Locale}&media_filter=${MediaFilter}`;
-
-                  Utilities.manageAPI(Endpoint, Callback, Resolve, Reject);
+                  Utilities.manageAPI(`${Gateway}/random?q=${Term}&key=${Key}&limit=${Limit}&contentfilter=${Filter}&locale=${Locale}&media_filter=${MediaFilter}`, Callback, Resolve, Reject);
             });
       };
 
       Methods.trending = function (Limit, Callback)
       {
             return new Promise((Resolve, Reject) => {
-                  const Endpoint = `https://api.tenor.com/v1/trending?key=${Key}&limit=${Limit}&contentfilter=${Filter}&locale=${Locale}&media_filter=${MediaFilter}`;
-
-                  Utilities.manageAPI(Endpoint, Callback, Resolve, Reject);
+                  Utilities.manageAPI(`${Gateway}/trending?key=${Key}&limit=${Limit}&contentfilter=${Filter}&locale=${Locale}&media_filter=${MediaFilter}`, Callback, Resolve, Reject);
             });
       };
 
       Methods.trending_terms = function (Callback)
       {
             return new Promise((Resolve, Reject) => {
-                  const Endpoint = `https://api.tenor.com/v1/trending_terms?key=${Key}`;
-
-                  Utilities.manageAPI(Endpoint, Callback, Resolve, Reject);
+                  Utilities.manageAPI(`${Gateway}/trending_terms?key=${Key}`, Callback, Resolve, Reject);
             });
       };
 
       Methods.categories = function (Callback)
       {
             return new Promise((Resolve, Reject) => {
-                  const Endpoint = `https://api.tenor.com/v1/categories?key=${Key}&contentfilter=${Filter}&locale=${Locale}`;
-
-                  Utilities.manageAPI(Endpoint, Callback, Resolve, Reject);
+                  Utilities.manageAPI(`${Gateway}/categories?key=${Key}&contentfilter=${Filter}&locale=${Locale}`, Callback, Resolve, Reject);
             });
       };
 
       Methods.category = function (Category, Limit, Callback)
       {
             return new Promise((Resolve, Reject) => {
-                  const Endpoint = `https://api.tenor.com/v1/search?tag=${Category}&limit=${Limit}&key=${Key}&contentfilter=${Filter}&locale=${Locale}`;
-
-                  Utilities.manageAPI(Endpoint, Callback, Resolve, Reject);
+                  Utilities.manageAPI(`${Gateway}/search?tag=${Category}&limit=${Limit}&key=${Key}&contentfilter=${Filter}&locale=${Locale}`, Callback, Resolve, Reject);
             });
       };
 
