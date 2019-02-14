578680b1f9fe93b127ab3557c9993149c7dda904
Jinzulen <Jinzu@protonmail.ch>
2019-02-14 13:55:35 +0100

    New route for search autocomplete and suggestions.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /**
  * @author Jinzulen
  * @license Apache 2.0
- * 
  * TenorJS - Lightweight NodeJS wrapper around the Tenor.com API.
  */
 
@@ -9,9 +8,20 @@ module.exports = function (Credentials)
 {
       const Methods = {}, Utilities = require("./utilities");
 
+      /**
+       * Check if the user is running an outdated version of TenorJS.
+       */
       Utilities.checkVersion();
 
+      /**
+       * Methods related to search querying.
+       */
       Methods.Search = require("./search")(Credentials, Utilities);
+
+      /**
+       * Functions related to search term autocompleting and suggestions.
+       */
+      Methods.Suggest = require("./suggest")(Credentials, Utilities);
 
       return Methods;
 };
