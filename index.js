3595dd0c9338572c266ac7ed81b78853d31ef7a9
Jin <Jinzu@protonmail.ch>
2019-02-20 03:06:23 +0100

    Removed clutter, cleaned code.

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
  * 
  * TenorJS - Lightweight NodeJS wrapper around the Tenor.com API.
  */
-const FS     = require("fs"),
-      Colors = require("colors");
+const Utilities = require("./src/Tools/Utilities");
 
 exports.client = function (Credentials)
 {
@@ -20,55 +19,34 @@ exports.client = function (Credentials)
             throw new Error ("Client configuration is not complete; please ensure all configuration parameters are satisfied (Key, Locale, Filter).");
       }
       
-      if (!Filters.includes(Credentials.Filter.toLowerCase()))
-      {
-            throw new Error ("Content filter level has to be one of these options: off, low, medium, high.");
-      }
-
-      if (Credentials.MediaFilter)
-      {
-            if (!MediaFilters.includes(Credentials.MediaFilter.toLowerCase()))
-            {
-                  throw new Error ("Media content filter has to be one of these options: basic, minimal.");
-            }
-      } else if (!Credentials.MediaFilter) {
-            Credentials.MediaFilter = "minimal"; 
-      }
-
+      /**
+       * Set default date format if user has set none.
+       */
       if (!Credentials.DateFormat) Credentials.DateFormat = "D/MM/YYYY - H:mm:ss A";
 
+      /**
+       * Check if the set content filter level abides by the available options.
+       */
+      if (!Filters.includes(Credentials.Filter.toLowerCase())) throw new Error ("Content filter level has to be one of these options: off, low, medium, high.");
+
+      /**
+       * Check if the media filter abides by the available options.
+       */
+      if (Credentials.MediaFilter && !MediaFilters.includes(Credentials.MediaFilter.toLowerCase()))
+      {
+            throw new Error ("Media content filter has to be one of these options: basic, minimal.");
+      } else if (!Credentials.MediaFilter) { Credentials.MediaFilter = "minimal"; }
+
+      /**
+       * Set API gateway and set content filter to lowercase.
+       */
       Credentials.Gate   = "https://api.tenor.com/v1";
       Credentials.Filter = Credentials.Filter.toLowerCase();
 
       /**
-       * Should probably move this elsewhere in the future, not a good
-       * idea having this much code and heavy-lifting in one place.
+       * Check status of TenorJS config.
        */
-      let Creds = JSON.stringify(Credentials);
-      
-      function writeConfig()
-      {
-            FS.writeFileSync("tenor_config.json", Creds, function (Error) {
-                  if (Error) throw Error;
-                  console.log(Colors.bold.green(`# [TenorJS] Changes have been made to the configuration file. Process should be restarted.`));
-                  process.exit(1);
-            });
-      }
-      
-      try
-      {
-            if (!FS.existsSync("tenor_config.json"))
-            {
-                  writeConfig();
-            } else {
-                  FS.readFile("tenor_config.json", "utf8", function (Error, Data) {
-                        if (Error) throw Error;
-                        if (Data !== Creds) writeConfig();
-                  });
-            }
-      } catch (E) {
-            throw E;
-      }
+      Utilities.checkConfig(JSON.stringify(Credentials));
 
       return require("./src")(Credentials);
 };
